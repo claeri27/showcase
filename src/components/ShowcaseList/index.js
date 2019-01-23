229645e1c48feffb9347084d926592ec170cc8ec
Christopher Laeri <claeri27@gmail.com>
2019-01-22 19:45:56 -0500

    added movie api

--- src/components/ShowcaseList/index.js
+++ src/components/ShowcaseList/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Grid, Row, Col } from '@smooth-ui/core-sc'
+import { Button, Grid, Row, Col } from '@smooth-ui/core-sc'
+import axios from 'axios'
 import logo from '../../logo.svg'
+// import { getMovies, getTopRated, getNowPlaying, getUpcoming } from '../../services/movieapi'
 
 const ShowcaseListWrapper = styled.div`
   margin-top: 50px;
@@ -21,6 +23,16 @@ const Spinner = styled.img`
 `;
 
 const ShowcaseList = () => {
+  const [movies, setMovies] = useState([])
+
+  const getNowPlaying = async () => {
+    if (movies) {
+      const BASE_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US`;
+      const resp = await axios.get(BASE_URL);
+      setMovies(resp.data.results)
+    }
+  }
+
   return (
     <ShowcaseListWrapper>
       <Grid fluid mt="20px" mb="20px">
@@ -32,6 +44,10 @@ const ShowcaseList = () => {
           mb="50px"
         >
           <Col>
+            <Button onClick={() => {
+              getNowPlaying()
+              console.log(movies);
+            }}>TEST MOVIES</Button>
             {/* <Spinner src={logo} alt='logo'/> */}
           </Col>
         </Row>
---
+++ src/services/movieapi.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+function getMovies(input) {
+  const BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US&query=${input}`;
+  const resp = axios.get(BASE_URL);
+  return resp;
+}
+
+function getTopRated() {
+  const BASE_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US`;
+  const resp = axios.get(BASE_URL);
+  return resp;
+}
+
+function getNowPlaying() {
+  const BASE_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US`;
+  const resp = axios.get(BASE_URL);
+  console.log(resp);
+  return resp;
+}
+
+function getUpcoming() {
+  const BASE_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US`;
+  const resp = axios.get(BASE_URL);
+  return resp;
+}
+
+function getCredits(id) {
+  const BASE_URL = 'https://api.themoviedb.org/3/movie/' + id + '/credits?&api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US';
+  const resp = axios.get(BASE_URL);
+  return resp;
+}
+
+export {
+  getMovies,
+  getTopRated,
+  getNowPlaying,
+  getUpcoming,
+  getCredits
+}
