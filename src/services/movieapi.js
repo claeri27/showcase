593e82722ee80fe81c6408b76aeacf64ba25e0dc
Christopher Laeri <claeri27@gmail.com>
2019-01-22 20:27:17 -0500

    updated env and api file

--- src/services/movieapi.js
+++ src/services/movieapi.js
@@ -1,32 +1,32 @@
 import axios from 'axios';
 
 function getMovies(input) {
-  const BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US&query=${input}`;
+  const BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${input}`;
   const resp = axios.get(BASE_URL);
   return resp;
 }
 
 function getTopRated() {
-  const BASE_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US`;
+  const BASE_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_API_KEY}&language=en-US`;
   const resp = axios.get(BASE_URL);
   return resp;
 }
 
 function getNowPlaying() {
-  const BASE_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US`;
+  const BASE_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.MOVIE_API_KEY}&language=en-US`;
   const resp = axios.get(BASE_URL);
   console.log(resp);
   return resp;
 }
 
 function getUpcoming() {
-  const BASE_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US`;
+  const BASE_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.MOVIE_API_KEY}&language=en-US`;
   const resp = axios.get(BASE_URL);
   return resp;
 }
 
 function getCredits(id) {
-  const BASE_URL = 'https://api.themoviedb.org/3/movie/' + id + '/credits?&api_key=aa4028a6dba37dd82b8e185fa521fe08&language=en-US';
+  const BASE_URL = 'https://api.themoviedb.org/3/movie/' + id + `/credits?&api_key=${process.env.MOVIE_API_KEY}&language=en-US`;
   const resp = axios.get(BASE_URL);
   return resp;
 }
